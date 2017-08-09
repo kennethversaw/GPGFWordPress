5e5898d63afb6131994df7c1e29d990a1cbeda56
kennethversaw <kennethversaw@gmail.com>
2017-08-09 11:41:48 -0500

    Updated configurations for Wordpress multisite

--- wp-config.php
+++ wp-config.php
@@ -102,7 +102,13 @@ $table_prefix  = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
-define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'gpgamefestival.azurewebsites.net');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /* That's all, stop editing! Happy blogging. */
 
 //Relative URLs for swapping across app service deployment slots 
